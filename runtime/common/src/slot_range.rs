e1860aa5b6bffc048d996aa6c57d64ef2c03a286
Alan <61774141+Alanshaw2048@users.noreply.github.com>
2020-10-10 15:07:04 +0800

    Update slot_range.rs

--- runtime/common/src/slot_range.rs
+++ runtime/common/src/slot_range.rs
@@ -17,6 +17,7 @@
 //! The SlotRange struct which succinctly handles the ten values that
 //! represent all sub ranges between 0 and 3 inclusive.
 
+
 use sp_std::{result, ops::Add, convert::{TryFrom, TryInto}};
 use sp_runtime::traits::CheckedSub;
 use codec::{Encode, Decode};
