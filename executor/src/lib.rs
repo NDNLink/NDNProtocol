a601c1495db9e976de08822439093abfd0036209
Allen <70961236+AllenChain@users.noreply.github.com>
2020-10-13 14:56:17 +0800

    Update lib.rs

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -2,6 +2,7 @@
 use sc_executor::native_executor_instance;
 pub use sc_executor::NativeExecutor;
 
+
 // Declare an instance of the native executor named `Executor`. Include the wasm binary as the
 // equivalent wasm code.
 native_executor_instance!(
