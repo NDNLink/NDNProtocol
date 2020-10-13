9ede6ee44f03c9f634ae0639e826e93882aa02e2
Alan <61774141+Alanshaw2048@users.noreply.github.com>
2020-10-13 14:49:33 +0800

    Update frame_system.rs

--- runtime/polkadot/src/weights/frame_system.rs
+++ runtime/polkadot/src/weights/frame_system.rs
@@ -17,6 +17,7 @@
 
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0-rc5
 
+
 #![allow(unused_parens)]
 
 use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
