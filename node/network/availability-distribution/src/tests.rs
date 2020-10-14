f3a054bf0868ff45bf29fd14512c99f720525fa9
Allen <70961236+AllenChain@users.noreply.github.com>
2020-10-14 15:32:03 +0800

    Update tests.rs

--- node/network/availability-distribution/src/tests.rs
+++ node/network/availability-distribution/src/tests.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
+
 use super::*;
 use assert_matches::assert_matches;
 use polkadot_erasure_coding::{branches, obtain_chunks_v1 as obtain_chunks};
