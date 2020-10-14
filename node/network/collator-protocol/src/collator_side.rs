da09e9e25b5335ed6217824e386017d1b9156453
Allen <70961236+AllenChain@users.noreply.github.com>
2020-10-14 15:35:02 +0800

    Update collator_side.rs

--- node/network/collator-protocol/src/collator_side.rs
+++ node/network/collator-protocol/src/collator_side.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
+
 use std::collections::HashMap;
 
 use futures::channel::oneshot;
