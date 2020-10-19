68ea407b31f3693ad1fa1eb782a9ed9515dcc40e
Allen <70961236+AllenChain@users.noreply.github.com>
2020-10-19 16:27:16 +0800

    Update sha1.rs

--- ring/src/digest/sha1.rs
+++ ring/src/digest/sha1.rs
@@ -13,6 +13,7 @@
 // OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 // CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+
 use super::sha2::{ch, maj, Word};
 use crate::c;
 use core::{convert::TryInto, num::Wrapping};
