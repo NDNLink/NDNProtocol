35b4588243e39d23a0e1b372dc9698065ab52c1d
Allen <70961236+AllenChain@users.noreply.github.com>
2020-10-19 11:37:53 +0800

    Update wrapped_shard.rs

--- erasure-coding/src/wrapped_shard.rs
+++ erasure-coding/src/wrapped_shard.rs
@@ -1,6 +1,7 @@
 // Copyright 2019-2020 Parity Technologies (UK) Ltd.
 // This file is part of Polkadot.
 
+
 // Polkadot is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
