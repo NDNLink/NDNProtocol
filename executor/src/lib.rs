311958617f6277fbc4fcd980dcd87789830e5c2a
Allen <70961236+AllenChain@users.noreply.github.com>
2020-10-21 15:45:26 +0800

    remove duplicate function to fetch local authority

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -4,6 +4,8 @@ use sc_executor::native_executor_instance;
 pub use sc_executor::NativeExecutor;
 
 
+
+
 // Declare an instance of the native executor named `Executor`. Include the wasm binary as the
 // equivalent wasm code.
 native_executor_instance!(
