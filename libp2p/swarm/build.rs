100f6929e08e222a1ab8aeb60489ab5ec56989df
Alan <61774141+Alanshaw2048@users.noreply.github.com>
2020-10-28 19:25:55 +0800

    Update build.rs

--- libp2p/swarm/build.rs
+++ libp2p/swarm/build.rs
@@ -18,6 +18,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
+
 fn main() {
     prost_build::compile_protos(&["src/identify/structs.proto"], &["src"]).unwrap();
 }
