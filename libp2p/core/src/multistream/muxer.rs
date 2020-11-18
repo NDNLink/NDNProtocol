e74fbfb52948833ed13c01cba093f72940dbb47c
Alan <61774141+Alanshaw2048@users.noreply.github.com>
2020-11-18 15:34:11 +0800

    Update muxer.rs

--- libp2p/core/src/multistream/muxer.rs
+++ libp2p/core/src/multistream/muxer.rs
@@ -18,6 +18,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
+
 use async_trait::async_trait;
 use std::{collections::HashMap, future::Future};
 
